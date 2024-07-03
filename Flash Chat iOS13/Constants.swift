e45010c73e0b6f81f12cc8ca225fe63c27dba22c
cagataykalkan <c.kalkan203@gmail.com>
2024-07-03 21:14:45 +0300

    full app

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Çağatay KALKAN on 28.06.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Views/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Çağatay KALKAN on 28.06.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
